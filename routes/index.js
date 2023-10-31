178c323428b29b97e2b6f35db1b2a1a39cf87ff5
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-10-31 16:22:11 +0000

    Add Form And On Submit Show New Message

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,16 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+router.post('/new', function(req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date()
+  })
+  res.redirect('/')
+
+})
 module.exports = router;
